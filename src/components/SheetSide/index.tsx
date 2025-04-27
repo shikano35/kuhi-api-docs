61fc81641a3ff6c751e9bbc959e2da4594b09cc2
takemura <170946913+shikano35@users.noreply.github.com>
2025-04-28 00:04:13 +0900

    feat: サイドシートコンポーネントを追加

---
+++ src/components/SheetSide/index.tsx
@@ -0,0 +1,40 @@
+import { Button } from '@/components/ui/button';
+import { Bars3Icon } from '@heroicons/react/24/outline';
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+
+export function SheetSide() {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <Bars3Icon className="size-5" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Edit profile</SheetTitle>
+          <SheetDescription>
+            Make changes to your profile here. Click save when you're done.
+          </SheetDescription>
+        </SheetHeader>
+        <div className="grid gap-4 py-4">
+
+        </div>
+        <SheetFooter>
+          <SheetClose asChild>
+            <Button type="submit">Save changes</Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  )
+}
