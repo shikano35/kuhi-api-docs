c32615c7bde645652029a54fe275437fee6e700b
takemura <170946913+shikano35@users.noreply.github.com>
2025-04-29 14:45:14 +0900

    style: コードスタイルの統一

--- src/components/ApiCarousel/index.tsx
+++ src/components/ApiCarousel/index.tsx
@@ -31,7 +31,7 @@ export const ApiCarousel: React.FC = () => {
               </p>
               <CardContent className="text-muted-foreground leading-12">
                 {ref.routes.map((route) => (
-                  <div key={route} className="mb-8 last:mb-4 flex flex-col">
+                  <div key={route} className="mb-8 flex flex-col last:mb-4">
                     <p className="text-primary text-base leading-10 font-semibold sm:text-lg">
                       {route}
                     </p>
