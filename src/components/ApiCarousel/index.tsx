9eb12fa25d7420cf338a378825440b90208b443e
takemura <170946913+shikano35@users.noreply.github.com>
2025-04-30 09:54:38 +0900

    fix: レスポンシブデザインの改善

--- src/components/ApiCarousel/index.tsx
+++ src/components/ApiCarousel/index.tsx
@@ -19,20 +19,20 @@ export const ApiCarousel: React.FC = () => {
         {apiReferences.map((ref) => (
           <CarouselItem
             key={ref.title}
-            className="flex-shrink-0 basis-md lg:basis-lg"
+            className="flex-shrink-0 basis-lg xl:basis-xl 2xl:basis-2xl"
           >
             <Card className="mx-8 flex h-full flex-col items-center p-8 select-none">
-              <ref.Icon className="h-16 w-16 sm:h-24 sm:w-24" />
-              <h3 className="text-primary text-2xl font-semibold sm:text-3xl">
+              <ref.Icon className="h-16 w-16 sm:h-24 sm:w-24 xl:h-32 xl:w-32" />
+              <h3 className="text-primary text-2xl font-semibold sm:text-3xl xl:text-4xl">
                 {ref.label}
               </h3>
-              <p className="text-muted-foreground -mt-4 text-sm sm:text-base">
+              <p className="text-muted-foreground -mt-4 text-sm sm:text-base xl:text-lg">
                 {ref.title}
               </p>
               <CardContent className="text-muted-foreground leading-12">
                 {ref.routes.map((route) => (
                   <div key={route} className="mb-8 flex flex-col last:mb-4">
-                    <p className="text-primary text-base leading-10 font-semibold sm:text-lg">
+                    <p className="text-primary text-base leading-10 font-semibold sm:text-lg xl:text-xl">
                       {route}
                     </p>
                     <p className="text-muted-foreground text-xs sm:text-sm">
